7c6e10a013f683fa15ff1e31323b8644f161a981
Tang Ming Feng <derpsnowtmf@gmail.com>
2021-01-31 13:32:39 +0800

    get global stats from api

--- script.js
+++ script.js
@@ -10,4 +10,22 @@ var settings = {
   
   $.ajax(settings).done(function (response) {
     console.log(response);
+    console.log(response.Global.NewConfirmed);
+  
+    /* Global stats */
+    let newConfirmed = response.Global.NewConfirmed;
+    let totalConfirmed = response.Global.TotalConfirmed;
+    let newDeaths = response.Global.NewDeaths;
+    let totalDeaths = response.Global.TotalDeaths;
+    let newRecovered = response.Global.NewRecovered;
+    let totalRecovered = response.Global.TotalRecovered;
+  
+  
+    var i;
+    for (i=0;i<response.Countries.length;i++){
+      if(response.Countries[i].Country == "Singapore"){
+        console.log("Singapore");
+        console.log(response.Countries[i]);
+      }
+    }
   });
