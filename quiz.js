d98b06ceea24720ab5062cd5bb24bb10c8bf9d49
Tang Ming Feng <derpsnowtmf@gmail.com>
2021-02-02 18:26:20 +0800

    fixes to quiz.js

--- quiz.js
+++ quiz.js
@@ -49,8 +49,8 @@ getNewQuestion = () => {
     }
 
     questionCounter ++;
-    progressText.innerText = Question ${questionCounter} of ${MAX_QUESTIONS};
-    progressBarFull.style.width = ${(questionCounter/MAX_QUESTIONS) * 100}%;
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`;
 
     const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionsIndex];
