35d9c80593d215c866297f211dfb878658c48939
Tang Ming Feng <derpsnowtmf@gmail.com>
2021-02-02 16:18:49 +0800

    created questions class for the quiz

---
+++ quiz.js
@@ -0,0 +1,18 @@
+let questions = [
+    {
+        question: 'What is Covid-19?',
+        choice1: 'Bacteria',
+        choice2: 'Virus',
+        choice3: 'Cell',
+        choice4: 'Device',
+        answer: 'Virus',
+    },
+    {
+        question: 'In what year did we find out about Covid-19?',
+        choice1: '2009',
+        choice2: '2016',
+        choice3: '2021',
+        choice4: '2019',
+        answer: '2019',
+    },
+]
