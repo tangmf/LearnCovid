ebd7d3545a02e0e305e82b3a72763995d0a3c3e6
Tang Ming Feng <derpsnowtmf@gmail.com>
2021-02-02 19:11:24 +0800

    fixes to quiz.js

--- quiz.js
+++ quiz.js
@@ -1,11 +1,11 @@
 // code was taken from https://youtu.be/f4fB9Xg2JEY
 
 
-const question = $("#question");
+const question = document.querySelector("#question");
 const choices = Array.from(document.querySelectorAll('.choice-text'));
-const progressText = $("#progressText");
-const scoreText = $("#score");
-const progressBarFull = $("#progressBarFull");
+const progressText = document.querySelector("#progressText");
+const scoreText = document.querySelector("#score");
+const progressBarFull = document.querySelector("#progressBarFull");
 
 let currentQuestion = {};
 let acceptingAnswers = true;
