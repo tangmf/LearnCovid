b178894252a3319da46e722c68d6965e77d496f1
josiahlow2@gmail.com <josiahlow2@gmail.com>
2021-02-02 19:57:03 +0800

    Re-did quiz.js

--- quiz.js
+++ quiz.js
@@ -1,22 +1,21 @@
-// code was taken from https://youtu.be/f4fB9Xg2JEY
+// // code was taken from https://youtu.be/f4fB9Xg2JEY
 
-
-const question = document.querySelector("#question");
+const question = document.querySelector('#question');
 const choices = Array.from(document.querySelectorAll('.choice-text'));
-const progressText = document.querySelector("#progressText");
-const scoreText = document.querySelector("#score");
-const progressBarFull = document.querySelector("#progressBarFull");
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
 
-let currentQuestion = {};
-let acceptingAnswers = true;
-let score = 0;
-let questionCounter = 0;
-let availableQuestions = [];
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
 
 let questions = [
     {
         question: 'What is Covid-19?',
-        choice1: 'Bacteria',
+        choice1: "Bacteria",
         choice2: 'Virus',
         choice3: 'Cell',
         choice4: 'Device',
@@ -48,57 +47,61 @@ let questions = [
     },
 ]
 
-const SCORE_POINTS = 100;
-const MAX_QUESTIONS = 4;
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
 
-startGame = () =>{
-    questionCounter = 0;
-    score = 0;
-    availableQuestions = [...questions];
-    getNewQuestion();
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
 }
-getNewQuestion = () => {
-    if(availableQuestions.Length === 0 || questionCounter > MAX_QUESTIONS){
-        localStorage.setItem('mostRecentScore', score);
 
-        return window.location.assign('/end.html');
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
     }
 
-    questionCounter ++;
-    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
-    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`;
-
-    const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionsIndex];
-    question.innerText = currentQuestion.question;
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+    
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
 
     choices.forEach(choice => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice ' + number ];
-    });
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
 
-    availableQuestions.splice(questionsIndex, 1);
+    availableQuestions.splice(questionsIndex, 1)
 
-    acceptingAnswers = true;
+    acceptingAnswers = true
 }
 
 choices.forEach(choice => {
     choice.addEventListener('click', e => {
         if(!acceptingAnswers) return
 
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset['number'];
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
 
         let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
-        if (classToApply === 'correct'){
+
+        if(classToApply === 'correct') {
             incrementScore(SCORE_POINTS)
         }
 
         selectedChoice.parentElement.classList.add(classToApply)
+
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply)
             getNewQuestion()
+
         }, 1000)
     })
 })
@@ -109,3 +112,4 @@ incrementScore = num => {
 }
 
 startGame()
+
