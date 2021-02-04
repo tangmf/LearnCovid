3e577863489c0f9234018da2091a3e15cdf47080
josiahlow2@gmail.com <josiahlow2@gmail.com>
2021-02-04 20:04:29 +0800

    changed some javascript to allow it to work

--- end.js
+++ end.js
@@ -1,8 +1,7 @@
 const username = document.querySelector('#username')
 const saveScoreBtn = document.querySelector('#saveScoreBtn');
 const finalScore = document.querySelector('#finalScore');
-const mostRecentScore = document.querySelector('#mostRecentScore');
-
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 const MAX_HIGH_SCORES = 5;
@@ -10,7 +9,7 @@ const MAX_HIGH_SCORES = 5;
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.Value;
+    saveScoreBtn.disabled = !username.value;
 })
 
 saveHighScore = e => {
@@ -30,7 +29,7 @@ saveHighScore = e => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
+    window.location.assign('highscores.html');
 }
 
 
