00b0b8a6615656a0d289b006b14ddd457e7a0356
josiahlow2@gmail.com <josiahlow2@gmail.com>
2021-02-06 22:31:52 +0800

    Trying to shrink the lottie with correct answer

--- quiz.js
+++ quiz.js
@@ -5,6 +5,7 @@ const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
+const lottie = document.querySelector('#lottie')
 
 let currentQuestion = {};
 let acceptingAnswers = true;
@@ -61,12 +62,13 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
 
-        return window.location.assign('/end.html');
+        return window.location.assign('end.html');
     }
 
     questionCounter++;
     progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
     progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`;
+    
     
     const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionsIndex];
@@ -93,9 +95,11 @@ choices.forEach(choice => {
         let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
         if(classToApply === 'correct') {
+            lottie.style.width = `${(questionCounter/MAX_QUESTIONS)}%`;
+            lottie.style.height = `${(questionCounter/MAX_QUESTIONS)}%`;
             incrementScore(SCORE_POINTS);
         }
-
+        
         selectedChoice.parentElement.classList.add(classToApply);
 
         setTimeout(() => {
@@ -103,6 +107,8 @@ choices.forEach(choice => {
             getNewQuestion();
 
         }, 1000)
+
+
     })
 })
 
@@ -110,6 +116,7 @@ incrementScore = num => {
     score +=num;
     scoreText.innerText = score;
 }
+
 
 startGame();
 
