1bde730c840aabdb5efa10b35b82d9aa222f28c2
josiahlow2@gmail.com <josiahlow2@gmail.com>
2021-02-09 15:03:11 +0800

    Added javascript for second fun fact container

--- learn.js
+++ learn.js
@@ -1,7 +1,12 @@
 $('#hidden').hide();
-
 $('#shown').click(function(){
     $('#shown').hide();
     $('#hidden').show();
-
 })
+
+$('#hidden2').hide();
+$('#shown2').click(function(){
+    $('#shown2').hide();
+    $('#hidden2').show();
+})
+
