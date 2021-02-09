3ed0735cfac686f09a7b57c01583b4dd7f2a0f6e
josiahlow2@gmail.com <josiahlow2@gmail.com>
2021-02-09 16:29:38 +0800

    Added javascript for the 2 new fun facts

--- learn.js
+++ learn.js
@@ -10,3 +10,14 @@ $('#shown2').click(function(){
     $('#hidden2').show();
 })
 
+$('#hidden3').hide();
+$('#shown3').click(function(){
+    $('#shown3').hide();
+    $('#hidden3').show();
+})
+
+$('#hidden4').hide();
+$('#shown4').click(function(){
+    $('#shown4').hide();
+    $('#hidden4').show();
+})
