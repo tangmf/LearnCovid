8a9914248caccc98cb82eec244f22f765e5132c6
josiahlow2@gmail.com <josiahlow2@gmail.com>
2021-02-15 14:45:00 +0800

    CSS and JS code fixes after going through the validator

--- learn.js
+++ learn.js
@@ -2,34 +2,34 @@ $('#hidden').hide();
 $('#shown').click(function(){
     $('#shown').hide();
     $('#hidden').show();
-})
+});
 
 $('#hidden2').hide();
 $('#shown2').click(function(){
     $('#shown2').hide();
     $('#hidden2').show();
-})
+});
 
 $('#hidden3').hide();
 $('#shown3').click(function(){
     $('#shown3').hide();
     $('#hidden3').show();
-})
+});
 
 $('#hidden4').hide();
 $('#shown4').click(function(){
     $('#shown4').hide();
     $('#hidden4').show();
-})
+});
 
 $('#hidden5').hide();
 $('#shown5').click(function(){
     $('#shown5').hide();
     $('#hidden5').show();
-})
+});
 
 $('#hidden6').hide();
 $('#shown6').click(function(){
     $('#shown6').hide();
     $('#hidden6').show();
-})
+});
